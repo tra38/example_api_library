5dc2933308f80d187be202e298bfdd54645c666f
tra38 <tra38@nau.edu>
2017-03-08 13:55:29 -0600

    Allow users to query information about propensity and ranking
    This is done by accessing the not_real.com API endpoint. Since not_real.com's API does not exist, we have to mock its existence.

--- customer_profile.rb
+++ customer_profile.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 require 'json'
+require_relative 'http_request'
 require 'pry'
-
 
 class CustomerProfile
   attr_reader :age, :income, :zipcode, :propensity, :ranking
@@ -10,6 +10,13 @@ class CustomerProfile
     @age = age
     @income = income
     @zipcode = zipcode
+    json = JSON.parse(HttpRequest.get_request(get_request_uri))
+    @propensity = json["propensity"]
+    @ranking = json["ranking"]
+  end
+
+  def get_request_uri
+    URI("https://not_real.com/customer_scoring?income=#{income}&zipcode=#{zipcode}&age=#{age}")
   end
 
 end
--- spec/customer_profile_spec.rb
+++ spec/customer_profile_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe CustomerProfile do
 
   context "generate a profile for a 35-year old with an income of $50,000 living in 6201" do
     before(:all) do
-      @library = CustomerProfile.new(age: 35, income: 50_000, zipcode: 6201)
+      RSpec::Mocks.with_temporary_scope do
+        json_object = %{{"propensity": 0.26532, "ranking": "C"}}
+        uri = URI("https://not_real.com/customer_scoring?income=50000&zipcode=6201&age=35")
+        allow(HttpRequest).to receive(:get_request).with(uri) { json_object }
+        @library = CustomerProfile.new(age: 35, income: 50_000, zipcode: 6201)
+      end
     end
 
     it "can generate a proper URI for not_real.com" do
