2cfb50f53cb745efbb1e57259a9b6e1a6419e418
tra38 <tra38@nau.edu>
2017-03-08 08:02:44 -0600

    Remove unnecessary questions
    We can just test the generated request URI to see whether the profile info is saved

--- spec/customer_profile_spec.rb
+++ spec/customer_profile_spec.rb
@@ -7,20 +7,6 @@ RSpec.describe CustomerProfile do
       @library = CustomerProfile.new(age: 35, income: 50_000, zipcode: 6201)
     end
 
-    # These tests may be unnecessary
-    it "can find age" do
-      expect(@library.age).to eq(35)
-    end
-
-    it "can find income" do
-      expect(@library.income).to eq(50_000)
-    end
-
-    it "can find zipcode" do
-      expect(@library.zipcode).to eq(6201)
-    end
-    # End unnecessary tests
-
     it "can generate a proper URI for not_real.com" do
       expect(@library.get_request_uri.query).to eq("income=50000&zipcode=6201&age=35")
     end
