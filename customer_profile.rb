bbe3c621cb2ad3c36658bf110c8282fff6cb622c
tra38 <tra38@nau.edu>
2017-03-10 15:41:17 -0600

    Modify the error message displayed for Internal Service Error
    Displays the http_request code now, not the entire response object.

--- customer_profile.rb
+++ customer_profile.rb
@@ -28,7 +28,7 @@ class CustomerProfile
     if http_request_code >= 200 && http_request_code < 400
       JSON.parse(http_request[:response])
     elsif http_request_code >= 500
-      raise "The API server is currently down. HTTP Response Code: #{http_request}"
+      raise "The API server is currently down. HTTP Response Code: #{http_request_code}."
     else
       raise "An unknown error has occurred. HTTP Response Code: #{http_request}"
     end
--- spec/customer_profile_spec.rb
+++ spec/customer_profile_spec.rb
@@ -25,16 +25,16 @@ RSpec.describe CustomerProfile do
     end
   end
 
-  context "Internal Service Error" do
-
+  context "5XX Client Error (Internal Service Error)" do
     it "raises a RuntimeError" do
       RSpec::Mocks.with_temporary_scope do
         html = "<html><body><h3>Internal Service Error</h3><br />The website is currently down. Please try again later.</body></html>"
         uri = URI("https://not_real.com/customer_scoring?income=50000&zipcode=6201&age=35")
         allow(HttpRequest).to receive(:get_request).with(uri) { { code: 500, response: html } }
-        expect { @library = CustomerProfile.new(age: 35, income: 50_000, zipcode: 6201) }.to  raise_error(RuntimeError)
+        expect { @library = CustomerProfile.new(age: 35, income: 50_000, zipcode: 6201) }.to  raise_error(RuntimeError, /The API server is currently down. HTTP Response Code: 500./)
       end
     end
+  end
 
   end
 
