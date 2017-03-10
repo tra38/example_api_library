28bb3874471b4dec3b6a44da97ee092e0ad47f1b
tra38 <tra38@nau.edu>
2017-03-10 16:12:24 -0600

    Silence console output during tests
    This makes the tests much easier to look at

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,20 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  # Silence console output during tests, from http://stackoverflow.com/a/15432948/4765379
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
+
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
